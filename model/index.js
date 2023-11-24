d82d3e0df8125c56a008271827d4b0e54e055f0e
DMT3772A <thomas.doumeng3112@gmail.com>
2023-11-24 19:29:39 +0100

    24/11/23:1930

--- model/index.js
+++ model/index.js
@@ -1,32 +1,97 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Load the data from the JSON file en fonction de la zone géographique sélectionné par l'utilisateur
-
+    // Load the data from the JSON file
     var zoneSelect = document.getElementById('zoneSelect');
-    zoneSelect.addEventListener('change', function() {
-        var zone = zoneSelect.value;
-        // console.log(zone);
+    var countrySelect = document.getElementById('countrySelect');
+    var experienceSelect = document.getElementById('experienceSelect');
+    var submit = document.getElementById('submit');
+
+    function getCountryList(zone) {
+        let countryList = [];
         $.ajax({
             url: '../data/survey_results_' + zone + '.json',
             dataType: 'json',
             success: function(data) {
                 var jsonData = data;
-                // console.log(jsonData)
+                
                 if(jsonData.length > 0 ) {
+
                     // Récupère la liste des pays dans le fichier JSON
-                    
+                    // console.log(jsonData)
+
                     jsonData.forEach(function(item){
-                        var country = item.EdLevel;
+                        var country = item.Country;
                         if (!countryList.includes(country)) {
                             countryList.push(country);
                         }     
                     });
-                    console.log(countryList);
+                    return countryList;
+                }else{
+                    return "No data";
+                }
+            }
+        });
+    }
+
+    console.log(getCountryList('NA'));
+
+    submit.addEventListener('click', function() {
+        var zone = zoneSelect.value;
+        var country = countrySelect.value;
+        var experience = experienceSelect.value;
+
+        $.ajax({
+            url: '../data/survey_results_' + zone + '.json',
+            dataType: 'json',
+            success: function(data) {
+                var jsonData = data;
+                
+                if( zone != '' && country != '' && experience != '' ) {
+
+                    // Récupère la liste des pays dans le fichier JSON
+                    // console.log(jsonData)
+
+                    jsonData.forEach(function(item){
+                        var country = item.Country;
+                        if (!countryList.includes(country)) {
+                            countryList.push(country);
+                        }     
+                    });
+                    // console.log(countryList);
+                    
                 }
             }
         });
     });
+
+    // Load the data from the JSON file en fonction de la zone géographique sélectionné par l'utilisateur
+
+    // zoneSelect.addEventListener('change', function() {
+    //     var zone = zoneSelect.value;
+    //     // console.log(zone);
+    //     $.ajax({
+    //         url: '../data/survey_results_' + zone + '.json',
+    //         dataType: 'json',
+    //         success: function(data) {
+    //             var jsonData = data;
+                
+    //             if(jsonData.length > 0 ) {
+    //                 // Récupère la liste des pays dans le fichier JSON
+    //                 // console.log(jsonData)
+
+    //                 jsonData.forEach(function(item){
+    //                     var country = item.Country;
+    //                     if (!countryList.includes(country)) {
+    //                         countryList.push(country);
+    //                     }     
+    //                 });
+    //                 console.log(countryList);
+    //             }
+    //         }
+    //     });
+    // });
+
 
 });
 
