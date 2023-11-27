b2efa2f6f037f0752112666bce8237a3d90fe973
Thomas Doumeng <thomas.doumeng3112@gmail.com>
2023-11-27 11:11:00 +0100

    update_exchange_rate_implementation

--- model/index.js
+++ model/index.js
@@ -1,32 +1,88 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Load the data from the JSON file en fonction de la zone gÈographique sÈlectionnÈ par l'utilisateur
+    // Load the data from the JSON file en fonction de la zone gÔøΩographique sÔøΩlectionnÔøΩ par l'utilisateur
 
     var zoneSelect = document.getElementById('zoneSelect');
+    var submitButton = document.getElementById('submitButton');
+    var countrySelect = document.getElementById('countrySelect');
+    var formationSelect = document.getElementById('formationSelect');
+
+    var countryListEU = [
+                        'United Kingdom of Great Britain and Northern Ireland',
+                        'Netherlands',
+                        'Germany',
+                        'France',
+                        'Spain',
+                        'Belgium',
+                        'Italy',
+                        'Portugal',
+                        'Switzerland',
+                        'Poland',
+                        'Ireland'
+                        ];
+
+    var countryListNA = ['United States of America', 'Canada'];
+
+    var formalEducationList = {
+        '': 'Choisir un niveau d\'√©tude',
+        'Primary/elementary school': 'Primary/Elementary school',
+        'Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)': 'Secondary School',
+        'Some college/university study without earning a degree': 'College / University without degree',
+        'Associate degree (A.A., A.S., etc.)': 'Associate degree',
+        'Bachelor\'s degree (B.A., B.S., B.Eng., etc.)': 'Bachelors degree',
+        'Master\'s degree (M.A., M.S., M.Eng., MBA, etc.)': 'Master\'s degree',
+        'Professional degree (JD, MD, Ph.D, Ed.D, etc.)': 'Professional degree',
+        'Something else': 'Others'
+      };
+
     zoneSelect.addEventListener('change', function() {
         var zone = zoneSelect.value;
-        // console.log(zone);
-        $.ajax({
-            url: '../data/survey_results_' + zone + '.json',
-            dataType: 'json',
-            success: function(data) {
-                var jsonData = data;
-                // console.log(jsonData)
-                if(jsonData.length > 0 ) {
-                    // RÈcupËre la liste des pays dans le fichier JSON
-                    
-                    jsonData.forEach(function(item){
-                        var country = item.EdLevel;
-                        if (!countryList.includes(country)) {
-                            countryList.push(country);
-                        }     
-                    });
-                    console.log(countryList);
-                }
-            }
-        });
+        // Vide countrySelect
+        countrySelect.innerHTML = '';
+
+        // Ajoute l'option "Veuillez s√©lectionner un pays"
+        var optionDefault = document.createElement('option');
+        optionDefault.value = '';
+        optionDefault.text = 'Veuillez s√©lectionner un pays';
+        countrySelect.appendChild(optionDefault);
+
+        if (zone === 'NA') {
+            countryListNA.forEach(function(country) {
+                var option = document.createElement('option');
+                option.value = country;
+                option.text = country;
+                countrySelect.appendChild(option);
+            });
+        } else {
+            countryListEU.forEach(function(country) {
+                var option = document.createElement('option');
+                option.value = country;
+                option.text = country;
+                countrySelect.appendChild(option);
+            });
+        }
+
+        // Ins√®re dans formationSelect les diff√©rentes formations possibles
+        formationSelect.innerHTML = '';
+        for (var key in formalEducationList) {
+            var option = document.createElement('option');
+            option.value = key;
+            option.text = formalEducationList[key];
+            formationSelect.appendChild(option);
+        }
+
     });
+
+    submitButton.addEventListener('click', function() {
+        // console.log(zoneSelect.value)    
+        if(zoneSelect.value != '' && countrySelect.value != '' && experienceSelect.value != '') {
+            console.log(zoneSelect.value);
+            console.log(countrySelect.value);
+            console.log(formationSelect.value);
+        }   
+    });
+
 
 });
 
